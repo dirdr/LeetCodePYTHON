1665d3989028e4c751c97d6d2a7f30fc87680078
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2021-03-26 09:06:19 +0100

    .lower()
    Runtime: 28 ms, faster than 77.36% of Python3 online submissions for To Lower Case.
    Memory Usage: 14.1 MB, less than 90.01% of Python3 online submissions for To Lower Case.

---
+++ Easy/709_to_lower_case.py
@@ -0,0 +1,6 @@
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
+
+# Runtime: 28 ms, faster than 77.36% of Python3 online submissions for To Lower Case.
+# Memory Usage: 14.1 MB, less than 90.01% of Python3 online submissions for To Lower Case.
